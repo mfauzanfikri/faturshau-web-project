1fd579f36f8722614807a87c63cc948a1724d631
mfauzanfikri <mfauzanfikri09@gmail.com>
2023-07-28 13:41:27 +0700

    adding bloghome-page and modifying home

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -25,7 +25,7 @@ const printBlogPost = (i: number) => {
 const page = () => {
   return (
     // section-1
-    <section className="bg-zinc-200 px-4 pb-6 pt-2">
+    <section className="bg-zinc-200 px-4 py-12">
       <div>
         <h1 className="text-3xl font-bold">Lorem ipsum dolor sit amet.</h1>
         <p className="mb-2 mt-1 text-xl">
@@ -36,15 +36,20 @@ const page = () => {
           exercitationem aut autem, illum suscipit esse?
         </p>
       </div>
-      <div>
-        <input
-          className="border-b border-b-zinc-400 bg-transparent p-3 text-sm focus:outline-none md:text-lg lg:w-3/5"
-          type="email"
-          placeholder=">>> Your Best Email"
-        />
-        <button className="text-light mt-4 rounded border border-zinc-400 bg-zinc-800 py-1.5 font-bold md:text-lg lg:mt-0 lg:px-8">
-          Subscribe
-        </button>
+
+      <div className="mt-4 flex flex-col gap-4">
+        <div>
+          <input
+            className="w-full rounded border border-zinc-400 bg-transparent p-3 text-sm focus:border-zinc-600"
+            type="email"
+            placeholder=">>> Your Best Email"
+          />
+        </div>
+        <div>
+          <button className="text-light w-full rounded border border-zinc-400 bg-zinc-800 py-1.5 font-bold">
+            Subscribe
+          </button>
+        </div>
       </div>
     </section>
   );
