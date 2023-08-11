556ffbab0b52449ac51e34b9be3880cc0d611dfa
mfauzanfikri <mfauzanfikri09@gmail.com>
2023-08-11 09:21:50 +0700

    editing next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  distDir: "dist",
+  // output: "export",
+  // images: {
+  //   unoptimized: true,
+  // },
+  // distDir: "dist",
 };
 
 module.exports = nextConfig;
